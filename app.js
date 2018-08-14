e074966f01335f953608792696fcadfdf03aa6b9
HugoMagalhaess <hugo.magalhaess@gmail.com>
2018-08-13 21:24:47 -0300

    Setting up Tooling
    Setting up static files (bootstrap)
    Adding new deppendencies.
    setting up coding  tools.
    Fixing code to ES6.

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-var express = require('express');
-var  chalk =  require('chalk');
-var  debug =  require('debug')('app');
-var morgan = require('morgan');
-var path =  require ('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+// app.use(morgan('combined'));
+app.use(morgan('tiny')); // another config
+app.use(express.static(path.join(__dirname, '/public/')));
 
 
-var app = express();
+// serving static files  to express
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-app.use(morgan('combined'));
-//app.use(morgan('tiny')); another config
-app.use(express.static(path.join(__dirname, '/public/')))
 /*
 to use debbug
 set DEBUG=* & node app.js
 set DEBUG=app & node app.js
 */
-app.get('/', function(req, res){
-   // res.send('Hello from my library app');
-    res.sendFile(path.join(__dirname, 'views/index.html') );
+app.get('/', (req, res) => {
+  // res.send('Hello from my library app');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, function() {
-    debug( `Listeninn on port  ${chalk.green('3000')}` );
-})
+app.listen(port, () => {
+  debug(`Listeninn at port  ${chalk.green(port)}`);
+});
