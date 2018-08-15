f872fef5cede7b95863d59fed6fcf3b2cda08727
HugoMagalhaess <hugo.magalhaess@gmail.com>
2018-08-14 23:21:10 -0300

    Templates
    index.pug removed.
    Started using .ejs.
    Getting  a new template.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 /*
 to use debbug
 set DEBUG=* & node app.js
@@ -26,7 +26,7 @@ set DEBUG=app & node app.js
 app.get('/', (req, res) => {
   // res.send('Hello from my library app');
   // res.sendFile(path.join(__dirname, 'views/index.html'));
-  res.render('index', { list: ['a', 'b', 'c', 'd', 'e'] });
+  res.render('index', { list: ['a', 'b', 'c', 'd', 'e'], title: 'Hannah' });
 });
 
 app.listen(port, () => {
